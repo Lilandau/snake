baec728fa050bad77d01a1b7725cb977767025e0
Amunet <dummy@web.de>
2022-08-27 21:21:40 +0200

    added 2d board

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import './Board.css';
 
+const BOARD_SIZE=10;
+
 function Board() {
+
+  const[board, setBoard] = useState( new Array(BOARD_SIZE).fill(0).map(row => new Array(BOARD_SIZE).fill(0)),);
+
+
     return (
-      <div>
-          Hallo Board
+      <div className="board">
+        {board.map((row, rowIdx)=> (
+          <div key={rowIdx} className='row'>{
+            row.map((cell, cellIdx) => (
+              <div key={cellIdx} className='cell'></div>
+            ))
+            }</div>
+        ))}
     </div>
     );
   }
